318b9e158c481b37428155a88dc8cca9ba9fb411
Swaye Chateau <swaye@kitechsoftware.com>
2022-02-15 01:07:25 -0500

    added flip move

--- components/Results.js
+++ components/Results.js
@@ -1,12 +1,12 @@
 import Thumbnail from "./Thumbnail";
-
+import FlipMove from "react-flip-move"
 const Results = ({ results }) => {
     return (
-    <div className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center gap-2">
+    <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center gap-2">
         {results.map(result => (
             <Thumbnail key={result.id}  result={result}/>
         ))}
-    </div>
+    </FlipMove>
   )
 }
 
--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,9 +1,10 @@
 import Image from "next/image"
 import { ThumbUpIcon } from "@heroicons/react/outline";
-const Thumbnail = ({result}) => {
+import { forwardRef } from "react";
+const Thumbnail = forwardRef(({result}, ref) => {
   const BASE_URL = "https://image.tmdb.org/t/p/original";
   return (
-    <div className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+    <div ref={ref} className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
         <Image layout="responsive" src={`${BASE_URL}${result.backdrop_path || result.poster_path}`} height={1080} width={1920}/>
         <div className="p-2">
             <p className="truncate max-w-md">{result.overview}</p>
@@ -17,6 +18,6 @@ const Thumbnail = ({result}) => {
         
     </div>
   )
-}
+})
 
 export default Thumbnail
